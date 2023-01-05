514bfb0fb5066fda549a2b8da8574122e5f2c832
mihirsuman0131@gmail.com <mihirsuman0131@gmail.com>
2023-01-05 20:42:24 +0530

    Initial Commit

---
+++ app/src/main/java/com/ultracures/bhardwajhospitals/MainActivity.java
@@ -0,0 +1,103 @@
+package com.ultracures.bhardwajhospitals;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.RelativeLayout;
+
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+
+    RelativeLayout internetLayout, noInternetLayout;
+    WebView webView;
+    ProgressBar pgBar;
+    Button tryAgainButton;
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Objects.requireNonNull(getSupportActionBar()).hide();
+
+        internetLayout = findViewById(R.id.internetLayout);
+        noInternetLayout = findViewById(R.id.noInternetLayout);
+        webView = findViewById(R.id.webView);
+        pgBar = findViewById(R.id.pgBar);
+        tryAgainButton = findViewById(R.id.tryAgainButton);
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setLoadWithOverviewMode(true);
+
+        webView.loadUrl("https://bhardwajhospitals.in/");
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onPageStarted(WebView view, String url, Bitmap favicon) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                pgBar.setVisibility(View.GONE);
+                super.onPageFinished(view, url);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                noInternetLayout.setVisibility(View.VISIBLE);
+                internetLayout.setVisibility(View.GONE);
+                super.onReceivedError(view, request, error);
+            }
+        });
+
+        tryAgainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                drawLayout();
+                webView.loadUrl("javascript:window.location.reload(true)");
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()){
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    public boolean isNetworkAvailable(){
+        ConnectivityManager manager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo info = manager.getActiveNetworkInfo();
+        return (info != null && info.isConnected());
+    }
+
+    public void drawLayout(){
+        if(isNetworkAvailable()){
+            internetLayout.setVisibility(View.VISIBLE);
+            noInternetLayout.setVisibility(View.GONE);
+            webView.loadUrl("javascript:window.location.reload(true)");
+        } else {
+            noInternetLayout.setVisibility(View.VISIBLE);
+            internetLayout.setVisibility(View.GONE);
+        }
+    }
+}
